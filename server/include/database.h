e4e4b99700550622dd3cca104c76bac7dee950e7
lizz-kuzz <lizzzkuzzz@gmail.com>
2024-04-26 17:14:55 +0300

    first steps in portagesql

---
+++ server/include/database.h
@@ -0,0 +1,10 @@
+#pragma once 
+
+#include <pqxx/pqxx>
+
+// using namespace pqxx; remember!!
+
+namespace Database {
+
+
+}//namespace Database
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,13 +4,32 @@
 #include <curl/curl.h>
 #include "http_request.h"
 #include "json_parsing.h"
+#include "database.h"
 
 int main() {
     std::string readBuffer;
 
     HTTP::Request weather;
-    weather.get_buffer_from_api(); //add here coord diffrent countries
+    weather.get_buffer_from_api(); //add here coord diffrent countries + bd
 
     Parsing::parse(weather.buffer);
+
+    pqxx::connection c{"postgresql://lizzkuzz:1511@localhost/testdb"};
+    //   pqxx::connection c{"postgresql://accounting@localhost/company"};
+    pqxx::work txn{c};
+
+  // For querying just one single value, the transaction has a shorthand method
+  // query_value().
+  //
+  // Use txn.quote() to escape and quote a C++ string for use as an SQL string
+  // in a query's text.
+    pqxx::result r{txn.exec("SELECT temp FROM table_temp;")};
+    for (auto row : r) {
+        std::cout
+      // Address column by name.  Use c_str() to get C-style string.
+        << row[0]
+      // Address column by zero-based index.  Use as<int>() to parse as int.
+        << std::endl;
+    }
 
 }
